12413b113d550d10f328f20c9d741be6290274cc
Mr. Coxall <mr.coxall@mths.ca>
2017-11-20 12:09:14 -0500

    fix
    Fixed the name of the splash screen and its size.

--- splash_scene.py
+++ splash_scene.py
@@ -23,9 +23,10 @@ class SplashScene(Scene):
                                      color = (0.61, 0.78, 0.87), 
                                      parent = self, 
                                      size = self.size)
-        self.school_crest = SpriteNode('./assets/sprites/MT_Game_Studio.jpg',
+        self.school_crest = SpriteNode('./assets/sprites/MT_Game_Studio.png',
                                        parent = self,
-                                       position = self.size/2)
+                                       position = self.size/2,
+                                       size = self.size)
     
     def update(self):
         # this method is called, hopefully, 60 times a second
